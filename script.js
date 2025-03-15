babafe3ffd41e6fa6ef370019c6ddb40f30608bf
onkar <upasenamrata14@gmail.com>
2025-03-16 00:46:16 +0530

    comented

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let URL=fetch("https://dummyjson.com/test").then((res)=>{
-   console.log(res.json());
-   ;
+// let URL=fetch("https://dummyjson.com/test").then((res)=>{
+//    console.log(res.json());
+//    ;
 
-})
+// })
  
