d6bfc51a56f3cfff7bcc0f7693235117841caf2a
onkar <upasenamrata14@gmail.com>
2025-03-16 17:52:02 +0530

    code

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let url="https://meme-api.com/gimme/wholesomememes"
 let btn=document.querySelector("button");
 let info=document.querySelector("h3");
+let img=document.querySelector("img");
+let author=document.querySelector("span")
 
+async function getMame() {
+    let obj= await axios.get(url);
+    console.log(obj.data);
+    img.setAttribute("src",obj.data.url)
+    console.log(obj.data.author);   
+    author.innerText=obj.data.author;
+    info.innerText=obj.data.title
+}
+btn.addEventListener("click",getMame);
